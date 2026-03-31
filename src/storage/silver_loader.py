77928e802c39544f0633913471861647361cf5ce
arunp061106 <arunp061106@gmail.com>
2026-03-31 17:14:21 +0530

    Ensure silver directory exists before loading data

--- src/storage/silver_loader.py
+++ src/storage/silver_loader.py
@@ -1,9 +1,9 @@
 import duckdb
-
+import os
 DB_PATH = "data/silver/synapse.db"
 
 def load_to_silver(df):
-
+    os.makedirs("data/silver",exist_ok=True)
     con = duckdb.connect(DB_PATH)
 
     con.register("df_temp", df)
